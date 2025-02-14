99373c0d69bf90ffd16227e734827d7849f59519
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2025-02-14 11:20:40 +0100

    Började med beskrivning av spelet metod att kunna konvertera användarens input till en int

---
+++ Battleships-projekt 1/Program.cs
@@ -0,0 +1,22 @@
+﻿void Spelbräda(){
+System.Console.WriteLine("Båt 1 har två rutor och båt 2 har tre rutor");
+System.Console.WriteLine("De ligger slumpmässigt över hela 4x4 spelplanen");
+SkrivEttNum(); 
+
+
+
+
+}
+int SkrivEttNum(){ 
+string notAnum = Console.ReadLine();
+    int isAnum;
+    while (!int.TryParse(notAnum, out isAnum))
+    {
+        System.Console.WriteLine("Skriv ett nummer");
+        notAnum = Console.ReadLine();
+    }
+    return isAnum;
+}
+
+
+Spelbräda();
