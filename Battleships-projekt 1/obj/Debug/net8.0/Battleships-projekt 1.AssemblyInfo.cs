e882d234554edee900a45192b4a99806ba9804b7
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2025-03-31 09:54:45 +0200

    commit, för att den sa påt mig att göra det

--- Battleships-projekt 1/obj/Debug/net8.0/Battleships-projekt 1.AssemblyInfo.cs
+++ Battleships-projekt 1/obj/Debug/net8.0/Battleships-projekt 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Battleships-projekt 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c358cebe885258279d3dbc9875d7c04658fd6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+727dcd9b1912e6dcfb338e82bdbc71a8403e09e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Battleships-projekt 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Battleships-projekt 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
